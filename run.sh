a4d644133b38d0eb6b8cc48f4a37c1408dbbc7bd
somkiat <somkiat.p@gmail.com>
2017-03-26 14:55:06 +0700

    Run server

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #git clone https://github.com/up1/demo_deploy_war.git
-cd demo_deploy_war
+#cd demo_deploy_war
 docker run  -d --name dbserver -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=carpark  mysql:latest
 docker container run -d --name webserver --link dbserver -p 8080:8080 -v $(pwd):/usr/local/tomcat/webapps tomcat:8-jre8
 apt install mysql-client-core-5.7
