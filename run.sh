78bbde77352e9bf09256bce629d532aaf3807bf3
somkiat <somkiat.p@gmail.com>
2017-03-26 14:54:26 +0700

    Run server

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/up1/demo_deploy_war.git
+#git clone https://github.com/up1/demo_deploy_war.git
 cd demo_deploy_war
 docker run  -d --name dbserver -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 -e MYSQL_DATABASE=carpark  mysql:latest
 docker container run -d --name webserver --link dbserver -p 8080:8080 -v $(pwd):/usr/local/tomcat/webapps tomcat:8-jre8
